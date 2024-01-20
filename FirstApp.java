3e09c06eb4e92afd487b73d09d16892d18ee4939
Lapkinu <lapkinu@gmail.com>
2024-01-20 13:24:00 +0200

    next

--- FirstApp.java
+++ FirstApp.java
@@ -1,5 +1,7 @@
 public class FirstApp {   
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere");
+        System.out.println("Write once, run anywhere123");
+
+
     }
 }
