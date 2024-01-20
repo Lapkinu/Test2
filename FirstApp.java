90259c274a6a8c98fd8a7dbfe85423ca4fc6734b
Lapkinu <lapkinu@gmail.com>
2024-01-20 14:27:03 +0200

    next

--- FirstApp.java
+++ FirstApp.java
@@ -1,7 +1,5 @@
 public class FirstApp {   
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere123");
-
-
+        System.out.println("Привет !!!");
     }
 }
